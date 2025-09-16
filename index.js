af8ebc67caf85a14a6c96e5447f7ca4ce8f21917
wangeshiizzai <keshi.yvonne84@gmail.com>
2025-09-16 11:30:30 +0300

    completed looping code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push("Thank you, " + names[i] + ", for the wonderful gift!")
+  }
+  return messages
+}
+
+function countDown(n) {
+  for (let i = n; i >= 0; i--) {
+    console.log(i)
+  }
+}
+
+module.exports = { writeCards, countDown }
+
